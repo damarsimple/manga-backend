d4fd30437fb0767986a088fc60c10388eb8e6e8a
Damar Albaribin <damaralbaribin@gmail.com>
2022-02-21 23:38:20 +0700

    fix : connection

--- src/scripts/jobs/check-size-migration.ts
+++ src/scripts/jobs/check-size-migration.ts
@@ -1,7 +1,10 @@
 import { QueueGetters } from "bullmq";
+import { connection } from "../../modules/Redis";
 
 async function main() {
-    const count = await new QueueGetters("chapter migration").count()
+    const count = await new QueueGetters("chapter migration", {
+        connection
+    }).count()
 
     console.log(`count ${count}`);
 
