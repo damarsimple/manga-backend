6b25ee939ae489e1a4737924e78accd50e7f6233
Damar Albaribin <damaralbaribin@gmail.com>
2022-02-22 04:11:59 +0700

    feat : redis modules

--- src/modules/Redis.ts
+++ src/modules/Redis.ts
@@ -1,6 +1,7 @@
 import IORedis from 'ioredis';
 
 export const connection = new IORedis({
+    host: "ssh.damaral.my.id",
     maxRetriesPerRequest: null,
     enableReadyCheck: false
 });
--- src/playground.ts
+++ src/playground.ts
@@ -6,9 +6,16 @@ async function main() {
 
     const axios = b.getAxios()
 
-    const d = await axios.get("https://sg.storage.bunnycdn.com/komikgudang/07-ghost/?download")
+    const start = new Date().getTime();
 
-    console.log(d.data)
+    const x = await b.download("https://cdn.gudangkomik.com/test.jpg");
+
+    console.log(`${new Date().getTime() - start}`);
+
+
+    // const d = await axios.get("https://sg.storage.bunnycdn.com/komikgudang/07-ghost/?download")
+
+    // console.log(d.data)
 
 
 }
