20782b9fb02b0f053af20679ce03f99ecc614dec
Damar Albaribin <damaralbaribin@gmail.com>
2022-02-22 04:16:22 +0700

    feat : more info

--- src/modules/Redis.ts
+++ src/modules/Redis.ts
@@ -2,6 +2,8 @@ import IORedis from 'ioredis';
 
 export const connection = new IORedis({
     host: "ssh.damaral.my.id",
+    password: "",
+    port: 6379,
     maxRetriesPerRequest: null,
     enableReadyCheck: false
 });
