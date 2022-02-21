a5987d829d3f5834d78f8c43b504c64d47933246
Damar Albaribin <damaralbaribin@gmail.com>
2022-02-21 23:26:35 +0700

    feat  : dl

--- src/scripts/jobs/migrate-to-do.ts
+++ src/scripts/jobs/migrate-to-do.ts
@@ -1,39 +1,106 @@
 import { prisma } from '../../modules/Context';
 import { chapterMigrationQueue } from '../../modules/Queue';
+import { gql, GraphQLClient } from "graphql-request";
+import { Chapter } from '@prisma/client';
+import { APP_ENDPOINT } from '../../modules/Env';
+import { SECRET_KEY } from '../../modules/Key';
+import sharp from 'sharp';
+import { DOSpaces } from '../../modules/DOSpaces';
 
 
+const sanityEclipse = gql`
+query Query($where: ChapterWhereInput, $take: Int, $skip: Int) {
+  findManyChapter(where: $where, take: $take, skip: $skip) {
+    name
+    id
+    imageUrls
+  }
+}
+`
 
+
+const updateChapter = gql`
+mutation UpdateOneChapter($data: ChapterUpdateInput!, $where: ChapterWhereUniqueInput!) {
+    updateOneChapter(data: $data, where: $where) {
+        id
+    }
+}
+
+`
+
+const compress = async (e: Buffer) => {
+    return await sharp(e).webp({
+        quality: 80
+    }).toBuffer()
+}
+
+
+const client = new GraphQLClient(APP_ENDPOINT, {
+    headers: {
+        authorization: SECRET_KEY
+    }
+})
+
+
+const t = new DOSpaces()
 
 const main = async () => {
 
 
 
 
-    const start = new Date().getTime()
-
-    let skip = 0;
+    let index = 0;
 
     while (true) {
+        const start = new Date().getTime()
 
-        const chapters = await prisma.chapter.findMany({
-            where: {
-                processed: false
+
+        const { findManyChapter: chapters } = await client.request<{
+            findManyChapter: Chapter[]
+        }>(sanityEclipse, {
+            "where": {
+                "processed": {
+                    "equals": false
+                },
             },
-            take: 100,
-            skip: 100 * skip
-        });
-
-        if (chapters.length == 0) break;
-
-        skip++;
-
+            "take": 200,
+            "skip": 200 * index
+        })
 
 
         for (const chapter of chapters) {
 
             try {
 
-                chapterMigrationQueue.add("chapter migration", { chapter })
+                // chapterMigrationQueue.add("chapter migration", { chapter })
+
+                const innerStart = new Date().getTime()
+
+
+                for (const img of chapter.imageUrls) {
+                    await t.downloadAndUpload(
+                        img.replace("https://cdn.gudangkomik.com/", "https://gudangkomik.b-cdn.net/"),
+                        img.replace("https://cdn.gudangkomik.com/", ""),
+                        compress
+                    )
+                }
+
+
+
+                const innerDiff = new Date().getTime() - innerStart;
+
+                await client.request(updateChapter, {
+                    "data": {
+                        "processed": {
+                            "set": true
+                        }
+                    },
+                    "where": {
+                        "id": chapter.id
+                    }
+                })
+
+                console.log(`Processed ${chapter.name} time ${innerDiff}`)
 
             } catch (error) {
                 console.log(error);
@@ -42,12 +109,17 @@ const main = async () => {
 
         }
 
+
+        const diff = new Date().getTime() - start;
+
+
+
+        console.log(`migrate-to-do finish ${index} at${diff}`);
+
+        index++;
+
     }
 
-    const diff = new Date().getTime() - start;
-
-
-    console.log(`migrate-to-do finish at${diff}`);
 }
 
 main()
