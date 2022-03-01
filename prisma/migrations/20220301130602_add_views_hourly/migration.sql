f2eb0ffda14e5f58a4c93b6a6fdeb7272076ecab
Damar Albaribin <damaralbaribin@gmail.com>
2022-03-01 20:45:33 +0700

    fix : invalid

--- prisma/migrations/20220301130602_add_views_hourly/migration.sql
+++ prisma/migrations/20220301130602_add_views_hourly/migration.sql
@@ -1,6 +1,3 @@
 -- AlterTable
-ALTER TABLE "Chapter" ADD COLUMN     "batchs" TEXT;
-
--- AlterTable
 ALTER TABLE "Comic" ADD COLUMN     "viewsDaily" INTEGER NOT NULL DEFAULT 0,
 ADD COLUMN     "viewsHourly" INTEGER NOT NULL DEFAULT 0;
