2adc25e1ba05fabdf7fe62b66ef9ace15c6196d7
Damar Albaribin <damaralbaribin@gmail.com>
2022-04-03 09:30:02 +0700

    fix : body size

--- src/server/graphql.ts
+++ src/server/graphql.ts
@@ -1,49 +1,45 @@
 import { makeSchema, fieldAuthorizePlugin } from "nexus";
 import { join } from "path";
-import * as  customtypes from "../models";
+import * as customtypes from "../models";
 import * as types from "../graphql";
 import { context, prisma } from "../modules/Context";
-import { paljs } from '@paljs/nexus';
-import { SECRET_KEY } from '../modules/Key';
-import { parseToken } from '../modules/JWT';
+import { paljs } from "@paljs/nexus";
+import { SECRET_KEY } from "../modules/Key";
+import { parseToken } from "../modules/JWT";
 import { User } from "@prisma/client";
-import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-import express from 'express';
-import http from 'http';
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import express from "express";
+import http from "http";
 import { graphqlUploadExpress } from "graphql-upload";
 
 export const schema = makeSchema({
-
   types: [types, customtypes], // 1
-  plugins: [paljs({
-    excludeFields: ['password'],
-  }), fieldAuthorizePlugin(),],
+  plugins: [
+    paljs({
+      excludeFields: ["password"],
+    }),
+    fieldAuthorizePlugin(),
+  ],
   outputs: {
     typegen: join(__dirname, "../../", "nexus-typegen.ts"), // 2
 
     schema: join(__dirname, "../..", "schema.graphql"), // 3
-
   },
 
-  contextType: {                                    // 1
+  contextType: {
+    // 1
 
-    module: join(__dirname, "../modules/Context.ts"),        // 2
+    module: join(__dirname, "../modules/Context.ts"), // 2
 
     export: "Context",
     // 3
-
   },
-
 });
-
-
-
-
 
 async function startApolloServer() {
   const app = express();
-  app.use(graphqlUploadExpress());
+  app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
   const httpServer = http.createServer(app);
 
   const server = new ApolloServer({
@@ -59,7 +55,7 @@ async function startApolloServer() {
       if (!token) return context;
 
       // Try to retrieve a user with the token
-      const user = (await parseToken<User>(token))
+      const user = await parseToken<User>(token);
 
       return {
         user,
@@ -70,43 +66,52 @@ async function startApolloServer() {
       };
     },
 
-    plugins: [{
-      // Fires whenever a GraphQL request is received from a client.
-      async requestDidStart(requestContext) {
-        if (requestContext.request.operationName == "IntrospectionQuery") return {};
-        const start = new Date().getTime()
+    plugins: [
+      {
+        // Fires whenever a GraphQL request is received from a client.
+        async requestDidStart(requestContext) {
+          if (requestContext.request.operationName == "IntrospectionQuery")
+            return {};
+          const start = new Date().getTime();
 
-        return {
-          async willSendResponse(requestContext) {
-            const end = new Date().getTime()
+          return {
+            async willSendResponse(requestContext) {
+              const end = new Date().getTime();
 
-            const elapsed = end - start;
+              const elapsed = end - start;
 
-            if (elapsed > 5000) {
-              await prisma.perfomanceAnalytic.create({
-                data: {
-                  operationName: requestContext.request.operationName ?? "unnamed",
-                  query: requestContext.request.query ?? "noquery",
-                  variables: JSON.stringify(requestContext?.request?.variables ?? {}),
-                  time: end - start,
-                }
-              })
-              // console.log(`Operation resolved! ${requestContext.operationName} ${end - start} ms`);
-            }
-          }
-        }
+              if (elapsed > 5000) {
+                await prisma.perfomanceAnalytic.create({
+                  data: {
+                    operationName:
+                      requestContext.request.operationName ?? "unnamed",
+                    query: requestContext.request.query ?? "noquery",
+                    variables: JSON.stringify(
+                      requestContext?.request?.variables ?? {}
+                    ),
+                    time: end - start,
+                  },
+                });
+                // console.log(`Operation resolved! ${requestContext.operationName} ${end - start} ms`);
+              }
+            },
+          };
+        },
       },
-    }]
-
+    ],
   });
 
-
-
   await server.start();
-  server.applyMiddleware({ app });
-  await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve));
+  server.applyMiddleware({
+    app,
+    bodyParserConfig: {
+      limit: "100mb",
+    },
+  });
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
-
 
 startApolloServer();
